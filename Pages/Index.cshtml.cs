b44f4f8ce118a218d82cdcd52d7be8f40a959854
SUPAT RATTANASUKSUN <supat.r@bu.ac.th>
2023-11-05 23:49:22 +0700

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using FinalProject.Areas.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Identity;
+
+namespace FinalProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public List<EmailInfo> listEmails = new List<EmailInfo>();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            try
+            {
+                String connectionString = "Server=tcp:buem.database.windows.net,1433;Initial Catalog=buem;Persist Security Info=False;User ID=supat;Password=U$er1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string username = User.Identity.Name;
+
+                    String sql = "SELECT * FROM emails WHERE emailreceiver='"+username+"'";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                EmailInfo emailInfo = new EmailInfo();
+                                emailInfo.EmailID = "" + reader.GetInt32(0);
+                                emailInfo.EmailSubject = reader.GetString(1);
+                                emailInfo.EmailMessage = reader.GetString(2);
+                                emailInfo.EmailDate = reader.GetDateTime(3).ToString();
+                                emailInfo.EmailIsRead = "" + reader.GetInt32(4);
+                                emailInfo.EmailSender = reader.GetString(5);
+                                emailInfo.EmailReceiver = reader.GetString(6);
+
+                                listEmails.Add(emailInfo);
+                            }
+                        }
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+    public class EmailInfo
+    {
+        public String EmailID;
+        public String EmailSubject;
+        public String EmailMessage;
+        public String EmailDate;
+        public String EmailIsRead;
+        public String EmailSender;
+        public String EmailReceiver;
+    }
+
+}
